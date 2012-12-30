59199253ea7a162bc0b2c4b5cda5bbe5e6d0db5c
Marcus Stollsteimer <sto.mar@web.de>
2012-12-30 21:30:40 +0100

    Add test for random index frequencies

--- test/test_rng.rb
+++ test/test_rng.rb
@@ -18,6 +18,23 @@ describe Nametrainer::RNG do
     indices.sort.last.must_be :<, @size
   end
 
+  it 'returns random indices with the expected frequency' do
+    samples = 100000
+    indices = Array.new(samples) { @rng.rand }
+
+    frequency = Hash.new {|h, k| h[k] = 0 }
+    indices.each do |index|
+      frequency[index] += 1
+    end
+    relative_frequency = frequency.sort.map {|ind, freq| freq.to_f/samples }
+
+    expected = [4.0/30, 4.0/30, 4.0/30, 3.0/30, 3.0/30, 3.0/30,
+                2.0/30, 2.0/30, 2.0/30, 1.0/30, 1.0/30, 1.0/30]
+    relative_frequency.each_with_index do |freq, index|
+      freq.must_be_within_epsilon expected[index], 0.05
+    end
+  end
+
   it 'can return a weighted index list' do
     expected = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2,
                 3, 3, 3, 4, 4, 4, 5, 5, 5,
